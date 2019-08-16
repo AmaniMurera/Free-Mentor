a150594ed0e9c13fa37dc00c360046712b2d2616
murera <amanidiope@gmail.com>
2019-08-16 12:24:56 +0200

    bug(fix-colors): improve UI colors
    change colors of navigation bar
    add a hover effects on the buttons
    add dynamic content using javascript
    [Finishes #167940905]

---
+++ UI/js/app.js
@@ -0,0 +1,56 @@
+	
+/* program toggle button */
+let navClose = document.getElementById('js-navbar-close');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+
+navBarToggle.addEventListener("click", function() {
+mainNav.classList.toggle("active");
+navBarToggle.classList.toggle("hide");
+navClose.classList.toggle("show");
+
+});
+navClose.addEventListener("click", function() {
+mainNav.classList.toggle("active");
+navBarToggle.classList.toggle("hide");
+navClose.classList.toggle("show");
+
+});
+
+
+/* view specific mentor */
+
+var myPopup = document.getElementById('myPopup');
+var modelbtn = document.getElementsByClassName("modelbtn");
+var span = document.getElementsByClassName("close")[0];
+
+for(var x = 0; x<15;x++){
+    modelbtn[x].onclick  = function(){
+        myPopup.style.display = "block";
+    }
+}
+ 
+
+
+ span.onclick  = function(){
+    myPopup.style.display = "none";
+}
+// window.onclick = function(e) {
+//     if (e.target == myPopup) {
+//         myPopup.style.display = "none";
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
