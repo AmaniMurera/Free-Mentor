cb7e89d5b9cca8e0b258e715030de1fad17d96cc
murera <amanidiope@gmail.com>
2019-08-31 12:00:33 +0200

    feature(login): add login endpoint
    implement json web token
    write unit test
    [Finishes #168232934]

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -103,6 +103,51 @@ class User {
 
       return newUser;
     };
+
+    login = (payload) => {
+      
+      const user = this.users.find((Wuser) => (Wuser.email === payload.email)
+         && ((Wuser.password === payload.password)));
+      if (!user) {
+        return {
+          status: status.UNAUTHORIZED,
+          error:
+         'email or password is incorrect!',
+        };
+      }
+
+      let result = {
+        token: generateAuthToken(user.id, user.is_admin, user.is_mentor),
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+      };
+      result = {
+        status: status.REQUEST_SUCCEDED,
+        message: 'User successfully signed in',
+        data: result,
+      };
+
+      return result;
+    };
+
+      isEmailTaken = (email) => this.users.find((u) => u.email === email);
+
+      isUserExist = (user_id) => this.users.find((u) => u.id === user_id);
+
+
+    checkMentor = (id) => {
+      const user = this.users.find((mentor) => mentor.id === parseInt(id, 10));
+      console.log(user.is_mentor);
+      if (user.is_mentor === true) { return true; }
+      return false;
+    }
+
+     // return user email
+     userEmail = (user_id) => {
+       const user = this.users.find((u) => u.id === parseInt(user_id, 10));
+       return user.email;
+     }
 }
 
 export default new User();
