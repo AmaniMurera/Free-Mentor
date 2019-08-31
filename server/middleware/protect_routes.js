c8bafa9bbb04a22fecfeeed052580f7da136cbcb
murera <amanidiope@gmail.com>
2019-08-31 18:14:33 +0200

    feature(reject session): Mentor should be able to reject a mentorship session requeat
    [Finishes #168233739]

--- server/middleware/protect_routes.js
+++ server/middleware/protect_routes.js
@@ -96,7 +96,6 @@ class Authorisation {
       );
     }
   }
-
 }
 
 export default Authorisation;
--- server/routes/session_route.js
+++ server/routes/session_route.js
@@ -9,14 +9,20 @@ const router = express.Router();
 
 // creation of object
 const session_controller = new SessionController();
-/ session creation
-router.post('/sessions',authorise.checkUser,session_controller.create);
+
+// session creation
+router.post('/sessions', authorise.checkUser, session_controller.create);
 
 // a mentor can view all mentorship request sessions created against him
-router.get('/sessions', authorise.checkMentor,session_controller.mentorViewAllSessionRequests);
+router.get('/sessions', authorise.checkMentor, session_controller.mentorViewAllSessionRequests);
 
 // a mentor can view a specific mentorship request sessions created against him
-router.get('/sessions/:id', authorise.checkMentor,session_controller.view_specific_session_request);
-// a mentor can accept a mentorship request sessions 
+router.get('/sessions/:id', authorise.checkMentor, session_controller.view_specific_session_request);
+// a mentor can accept a mentorship request sessions
 router.patch('/sessions/:id/accept', session_controller.acceptSession);
-// a mentor can reject a mentorship request sessions 
+// a mentor can reject a mentorship request sessions
+router.patch('/sessions/:id/reject', session_controller.rejectSession);
+
+// a mentee can view all mentorship request sessions created by him
+router.get('/sessions', authorise.checkUser, session_controller.menteeViewAllSessionRequests);
+export default router;
