a562b77326ff54efdc3cb3aade3e66f5023e0bdb
murera <amanidiope@gmail.com>
2019-09-03 09:23:54 +0200

    bg(UI feedback): implement UI feedback
    improve form appearance
    add page for mentor to accept and decline mentorship session
    replace lorem ipsum text with real text
    
    [Finishes #168250575]

--- UI/js/app.js
+++ UI/js/app.js
@@ -44,6 +44,25 @@ for(var x = 0; x<15;x++){
 
 
 
+let dialogbox = (message) => { // Get the modal
+    const modal = document.querySelector('#dialogbox');
+  
+    const divMsg = document.querySelector('.dialog-content-js');
+  
+  
+    divMsg.textContent = message;
+    // Display the modal
+    modal.style.display = 'block';
+  };
+  // Function close the dialog box
+  const closeDialog = () => {
+    document.querySelector('.modal').style.display = 'none';
+    const modal = document.querySelector('#dialogbox');
+    modal.style.display = 'none';
+  };
+
+
+
 
 
 
