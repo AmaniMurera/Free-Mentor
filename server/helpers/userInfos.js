69b1b87fe8c682399b32519ef1bad14e916e9bf6
murera <38111967+murera@users.noreply.github.com>
2019-09-06 10:17:36 +0200

    #168316693 upgrade readme file (#41)
    * bug(readme): upgrade readme file
    
    change location of badges on readme file
    
    [Finishes #168316693]
    
    * bug(comment): remove comments on PR
    
    [Finishes #168316693]
    
    * bug(comments) : remove comment on PR
    
    [Finishes #168316693]

--- server/helpers/userInfos.js
+++ server/helpers/userInfos.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 import status from './StatusCode';
 
 const getUserId = (res, token) => {
+  
   try {
     const decoded = jwt.verify(token, process.env.Token_Key);
     return decoded.id;
   } catch (error) {
-    // eslint-disable-next-line max-len
-    return res.status(status.BAD_REQUEST).send({ status: status.BAD_REQUEST, error: error.message });
+    return res.status(status.BAD_REQUEST).send({ status: 400, error: error.message });
   }
 };
 
--- server/routes/session_route.js
+++ server/routes/session_route.js
@@ -7,21 +7,17 @@ const authorise = new Authorisation();
 const router = express.Router();
 
 
-// creation of object
-const session_controller = new SessionController();
+router.post('/', authorise.checkUser, SessionController.create);
 
-// session creation
-router.post('/', authorise.checkUser, session_controller.create);
 
-// a mentor can view all mentorship request sessions created against him
-router.get('/', authorise.checkMentor, session_controller.mentorViewAllSessionRequests);
+router.get('/', authorise.checkMentor, SessionController.mentorViewAllSessionRequests);
 
-// a mentor can view a specific mentorship request sessions created against him
-router.get('/:id', authorise.checkMentor, session_controller.view_specific_session_request);
-// a mentor can accept a mentorship request sessions
-router.patch('/:id/accept', session_controller.acceptSession);
-// a mentor can reject a mentorship request sessions
-router.patch('/:id/reject', session_controller.rejectSession);
+
+router.get('/:id', authorise.checkMentor, SessionController.view_specific_session_request);
+
+router.patch('/:id/accept', SessionController.acceptSession);
+
+router.patch('/:id/reject', SessionController.rejectSession);
 
 
 export default router;
