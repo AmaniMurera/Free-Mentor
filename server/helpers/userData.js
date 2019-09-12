6365aa6768d4365ec6e0984e32a58b64083c22b1
murera <amanidiope@gmail.com>
2019-09-12 12:51:59 +0200

    improve test

--- server/helpers/userData.js
+++ server/helpers/userData.js
@@ -5,7 +5,7 @@ import status from './StatusCode';
 dotenv.config();
 export const userId = (token, res) => {
   try {
-    const decoded = jwt.verify(token, 'process.env.secretKey');
+    const decoded = jwt.verify(token, process.env.secretKey);
     return decoded.id;
   } catch (error) {
     return res.status(status.BAD_REQUEST).send({ status: 400, error: error.message });
@@ -13,7 +13,7 @@ export const userId = (token, res) => {
 };
 export const userEmail = (token, res) => {
   try {
-    const decoded = jwt.verify(token, 'process.env.secretKey');
+    const decoded = jwt.verify(token, process.env.secretKey);
     return decoded.email;
   } catch (error) {
     return res.status(status.BAD_REQUEST).send({ status: 400, error: error.message });
